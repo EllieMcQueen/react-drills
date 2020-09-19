e40d3f0c7decdd77145ad29c7f6e73549ad68aaa
Ellie McQueen <elliemcqueen@gmail.com>
2020-09-19 10:32:36 -0600

    react drills update

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import "./App.css";
+import Image from "./image.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Image url={"https://s23444.pcdn.co/wp-content/uploads/2019/05/Fairy-Glen-Skye-unsp.jpg.optimal.jpg"} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
